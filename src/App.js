a0d4e70f7f9c757bcc448a37ddc866729571bc7f
Hung Dinh <baohungdinhnguyen1412@gmail.com>
2023-06-10 10:49:21 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import HelloFunctionalComponent from './Component/HelloFunctionalComponent';
 // import DemonStateClass from './Component/DemoStateClassComponent';
 // import HelloClassComponent from './Component/HelloClassComponent';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <HelloClassComponent/> */}
-      {/* <DemonStateClass /> */}
-      <HelloFunctionalComponent name="Hung"/>
-    </div>
-  );
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
