4fb2c0aa1e094bda9793266d493bf322662cce70
Hung Dinh <baohungdinhnguyen1412@gmail.com>
2023-06-10 10:44:34 +0700

    Initial Setup

---
+++ src/Component/HelloFunctionalComponent.js
@@ -0,0 +1,7 @@
+const HelloFunctionalComponent = (props) => {
+    return (
+        <div><h1>Hello from functional component {props.name}</h1></div>
+    )
+}
+
+export default HelloFunctionalComponent;
