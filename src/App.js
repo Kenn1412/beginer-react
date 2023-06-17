499d98bd3bdd31d2c0a4c4a5d59d6ad9340f0a70
Kenn1412 <baohungdinhnguyen1412@gmail.com>
2023-06-17 10:56:44 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { DISHES } from './shared/dishes';
 
+/* Ex 25
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +23,21 @@ class App extends Component {
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+*/
+
+// ex 26
+import Main from './components/MainComponent';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
       </div>
     );
   }
